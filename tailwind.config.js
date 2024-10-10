3e1fe82cb6c1acfc3aa4e0d779506523f462c366
rin! <phurinmeng@gmail.com>
2024-10-10 15:20:25 -0700

    Add navigation bar and enhance styling with Tailwind CSS utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.glass-amber': {
+          '@apply bg-amber-800  backdrop-filter backdrop-blur-lg shadow-lg bg-opacity-50': {},
+        },
+        '.glass-slate': {
+          '@apply bg-slate-800 backdrop-filter backdrop-blur-lg shadow-lg bg-opacity-50': {},
+        },
+        '.mp5': {
+          '@apply m-5 p-5': {},
+        },
+        '.t200e': {
+          '@apply transition-all duration-200 ease-in-out': {},
+        }
+      });
+    }
+  ],
 }
